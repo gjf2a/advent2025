8ca5e6b4e8808d0fa3af9d6954d2d70055c29c79
gjf2a <ferrer@hendrix.edu>
2025-12-01 13:06:22 -0600

    Part 2 works!

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,25 +1,36 @@
-use advent2025::{advent_main, all_lines, Part};
+use advent2025::{Part, advent_main, all_lines};
 use anyhow::bail;
 use bare_metal_modulo::ModNumC;
 
 fn main() -> anyhow::Result<()> {
-    advent_main(|filename, part, _| {
-        match part {
-            Part::One => {
-                let mut position = ModNumC::<i16, 100>::new(50);
-                let mut zeros = 0;
-                for line in all_lines(filename).unwrap() {
-                    position += parse_line(line.as_str())?;
+    advent_main(|filename, part, _| match part {
+        Part::One => {
+            let mut position = ModNumC::<i16, 100>::new(50);
+            let mut zeros = 0;
+            for line in all_lines(filename).unwrap() {
+                position += parse_line(line.as_str())?;
+                if position == 0 {
+                    zeros += 1;
+                }
+            }
+            println!("{zeros}");
+            Ok(())
+        }
+        Part::Two => {
+            let mut position = ModNumC::<i16, 100>::new(50);
+            let mut zeros = 0;
+            for line in all_lines(filename).unwrap() {
+                let instruction = parse_line(line.as_str())?;
+                let update = if instruction < 0 { -1 } else { 1 };
+                for _ in 0..instruction.abs() {
+                    position += update;
                     if position == 0 {
                         zeros += 1;
                     }
                 }
-                println!("{zeros}");
-                Ok(())
             }
-            Part::Two => {
-                todo!("No part 2 yet")
-            }
+            println!("{zeros}");
+            Ok(())
         }
     })
 }
@@ -29,6 +40,6 @@ fn parse_line(line: &str) -> anyhow::Result<i16> {
     match &line[0..1] {
         "L" => Ok(-num),
         "R" => Ok(num),
-        _ => bail!("Unrecognized")
+        _ => bail!("Unrecognized"),
     }
 }
