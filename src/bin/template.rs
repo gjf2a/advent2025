2228640d8a2f31eaa462f4003d0303f730d87fb2
gjf2a <ferrer@hendrix.edu>
2025-12-07 03:47:20 -0600

    Made template easier to work with

--- src/bin/template.rs
+++ src/bin/template.rs
@@ -1,12 +1,15 @@
 use advent2025::{Part, advent_main, all_lines};
 
 fn main() -> anyhow::Result<()> {
-    advent_main(|filename, part, _| match part {
-        Part::One => {
-            todo!("No part 1 yet")
+    advent_main(|filename, part, _| {
+        match part {
+            Part::One => {
+                todo!("No part 1 yet")
+            }
+            Part::Two => {
+                todo!("No part 2 yet")
+            }
         }
-        Part::Two => {
-            todo!("No part 2 yet")
-        }
+        Ok(())
     })
 }
