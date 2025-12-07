89b084d8ebc3a69642f64be20e78f5121d8ffeab
gjf2a <ferrer@hendrix.edu>
2025-12-07 04:02:22 -0600

    Part 2: Overcounts on example.

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,5 +1,6 @@
 use advent2025::{Part, advent_main, grid::GridCharWorld, multidim::Position};
 use common_macros::b_tree_set;
+use hash_histogram::HashHistogram;
 use itertools::Itertools;
 
 fn main() -> anyhow::Result<()> {
@@ -10,7 +11,7 @@ fn main() -> anyhow::Result<()> {
                 println!("{}", count_beam_splits(&world));
             }
             Part::Two => {
-                todo!("No part 2 yet")
+                println!("{}", count_many_worlds_splits(&world));
             }
         }
         Ok(())
@@ -46,4 +47,29 @@ fn count_beam_splits(world: &GridCharWorld) -> u64 {
         }
     }
     split_count
+}
+
+fn count_many_worlds_splits(world: &GridCharWorld) -> u64 {
+    let s = start(world);
+    let start_row = s[1] as usize;
+    let mut beam_counts: HashHistogram<usize, u64> = HashHistogram::new();
+    beam_counts.bump(&(s[0] as usize));
+    let mut active_beams = b_tree_set![s[0] as usize];
+    for y in start_row..(world.height() - 1) {
+        let splitters = active_beams
+            .iter()
+            .filter(|x| world.get(**x, y).unwrap() == '^')
+            .copied()
+            .collect_vec();
+        for splitter in splitters {
+            assert!(splitter > 0);
+            assert!(splitter + 1 < world.width());
+            active_beams.remove(&splitter);
+            active_beams.insert(splitter - 1);
+            beam_counts.bump(&(splitter - 1));
+            active_beams.insert(splitter + 1);
+            beam_counts.bump(&(splitter + 1))
+        }
+    }
+    beam_counts.total_count()
 }
