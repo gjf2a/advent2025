80f59238bd255626d69d015849c26798129f4d4f
gjf2a <ferrer@hendrix.edu>
2025-12-09 22:15:27 -0600

    Part 2 works on example but fails on puzzle input.

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,16 +1,14 @@
-use std::{
-    cmp::{max, min},
-    collections::HashSet,
-};
+use std::
+    cmp::{max, min}
+;
 
 use advent2025::{Part, advent_main, all_lines, multidim::Point, sub_abs};
 use itertools::Itertools;
 
 type Corner = Point<u64, 2>;
-type Edge = (Corner, Corner);
 
 fn main() -> anyhow::Result<()> {
-    advent_main(|filename, part, _| {
+    advent_main(|filename, part, options| {
         let red_tiles = get_red_tiles(filename)?;
         let rects = all_rectangles(&red_tiles);
         match part {
@@ -24,6 +22,13 @@ fn main() -> anyhow::Result<()> {
             }
             Part::Two => {
                 let edges = RedEdges::new(&red_tiles);
+                if options.contains(&"-approved") {
+                    let approved = rects
+                        .iter()
+                        .filter(|(p1, p2)| edges.rectangle_approved(p1, p2))
+                        .collect_vec();
+                    println!("{approved:?}");
+                }
                 let biggest = rects
                     .iter()
                     .filter(|(p1, p2)| edges.rectangle_approved(p1, p2))
@@ -109,7 +114,9 @@ impl RedEdges {
 
     fn is_green(&self, corner: &Corner) -> bool {
         let mut inside = false;
+        //println!("corner: {corner}");
         for edge in self.edges_at(corner[1]) {
+            //println!("edge {edge:?}");
             match edge.tilt {
                 Tilt::Horizontal => {
                     inside = true;
@@ -127,6 +134,7 @@ impl RedEdges {
                         if corner[0] == edge.level {
                             return true;
                         } else if corner[0] < edge.level {
+                            //println!("In here: {corner}");
                             return false;
                         }
                         inside = true;
@@ -134,6 +142,7 @@ impl RedEdges {
                 }
             }
         }
+        //println!("end: {corner}");
         inside
     }
 
@@ -145,16 +154,17 @@ impl RedEdges {
                 Tilt::Vertical => edge.start <= y && y <= edge.end,
             })
             .copied()
+            .sorted()
     }
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Tilt {
     Horizontal,
     Vertical,
 }
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, Ord)]
 struct LevelInterval {
     level: u64,
     start: u64,
@@ -168,172 +178,17 @@ impl LevelInterval {
     }
 }
 
-// Older Part 2 ideas
-
-fn no_crossing_rectangles(red_tiles: &Vec<Corner>) -> HashSet<(Corner, Corner)> {
-    let mut result = HashSet::new();
-    for i in 0..red_tiles.len() {
-        for j in i + 1..red_tiles.len() {
-            let edges = rectangle_edges(&red_tiles[i], &red_tiles[j]);
-            let mut count = 0;
-            for edge in edges.iter() {
-                count += crossings(edge, red_tiles).count();
+impl PartialOrd for LevelInterval {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        match self.tilt {
+            Tilt::Horizontal => match other.tilt {
+                Tilt::Horizontal => self.start.partial_cmp(&other.start),
+                Tilt::Vertical => self.start.partial_cmp(&other.level),
             }
-
-            if count == 0 {
-                result.insert((red_tiles[i], red_tiles[j]));
+            Tilt::Vertical => match other.tilt {
+                Tilt::Horizontal => self.level.partial_cmp(&other.start),
+                Tilt::Vertical => self.level.partial_cmp(&other.level),
             }
         }
-    }
-    result
-}
-
-fn no_inside_rectangles(red_tiles: &Vec<Corner>) -> Vec<(Corner, Corner)> {
-    let mut result = vec![];
-    for i in 0..red_tiles.len() {
-        for j in i + 1..red_tiles.len() {
-            let rect = (red_tiles[i], red_tiles[j]);
-            if !red_tiles.iter().any(|p| contains(&rect, p)) {
-                result.push(rect);
-            }
-        }
-    }
-    result
-}
-
-fn rectangle_edges(p1: &Corner, p2: &Corner) -> [Edge; 4] {
-    let p12 = Corner::new([p1[0], p2[1]]);
-    let p21 = Corner::new([p2[0], p1[1]]);
-    [(*p1, p12), (p12, *p2), (*p2, p21), (p21, *p1)]
-}
-
-fn contains(rect: &(Corner, Corner), p: &Corner) -> bool {
-    let min_x = min(rect.0[0], rect.1[0]);
-    let max_x = max(rect.0[0], rect.1[0]);
-    let min_y = min(rect.0[1], rect.1[1]);
-    let max_y = max(rect.0[1], rect.1[1]);
-    min_x < p[0] && p[0] < max_x && min_y < p[1] && p[1] < max_y
-}
-
-fn crossings(edge: &Edge, red_tiles: &Vec<Corner>) -> impl Iterator<Item = Edge> {
-    (0..red_tiles.len() - 1)
-        .map(|i| (red_tiles[i], red_tiles[i + 1]))
-        .filter(|target| crosses(edge, target))
-}
-
-#[derive(Copy, Clone, PartialEq, Eq)]
-enum Segment {
-    Horizontal { y: u64, x_start: u64, x_end: u64 },
-    Vertical { x: u64, y_start: u64, y_end: u64 },
-}
-
-impl Segment {
-    fn of(edge: &Edge) -> Option<Self> {
-        if edge.0 == edge.1 {
-            None
-        } else if edge.0[0] == edge.1[0] {
-            Some(Self::Vertical {
-                x: edge.0[0],
-                y_start: min(edge.0[1], edge.1[1]),
-                y_end: max(edge.0[1], edge.1[1]),
-            })
-        } else if edge.0[1] == edge.1[1] {
-            Some(Self::Horizontal {
-                y: edge.0[1],
-                x_start: min(edge.0[0], edge.1[0]),
-                x_end: max(edge.0[0], edge.1[0]),
-            })
-        } else {
-            None
-        }
-    }
-
-    fn perpendicular(&self, other: &Self) -> bool {
-        match self {
-            Segment::Horizontal {
-                y: _,
-                x_start: _,
-                x_end: _,
-            } => match other {
-                Segment::Horizontal {
-                    y: _,
-                    x_start: _,
-                    x_end: _,
-                } => false,
-                Segment::Vertical {
-                    x: _,
-                    y_start: _,
-                    y_end: _,
-                } => true,
-            },
-            Segment::Vertical {
-                x: _,
-                y_start: _,
-                y_end: _,
-            } => match other {
-                Segment::Horizontal {
-                    y: _,
-                    x_start: _,
-                    x_end: _,
-                } => true,
-                Segment::Vertical {
-                    x: _,
-                    y_start: _,
-                    y_end: _,
-                } => false,
-            },
-        }
-    }
-
-    fn crosses(&self, other: &Self) -> bool {
-        match self {
-            Segment::Horizontal { y, x_start, x_end } => match other {
-                Segment::Horizontal {
-                    y: _,
-                    x_start: _,
-                    x_end: _,
-                } => false,
-                Segment::Vertical { x, y_start, y_end } => {
-                    x_start < x && x < x_end && y_start < y && y < y_end
-                }
-            },
-            Segment::Vertical { x, y_start, y_end } => match other {
-                Segment::Horizontal { y, x_start, x_end } => {
-                    x_start < x && x < x_end && y_start < y && y < y_end
-                }
-                Segment::Vertical {
-                    x: _,
-                    y_start: _,
-                    y_end: _,
-                } => false,
-            },
-        }
-    }
-}
-
-fn crosses(source: &Edge, target: &Edge) -> bool {
-    if let Some(src) = Segment::of(source) {
-        if let Some(targ) = Segment::of(target) {
-            return src.crosses(&targ);
-        }
-    }
-    false
-}
-
-fn is_green_tile_edge(red_tiles: &Vec<Corner>, candidate: &Corner) -> bool {
-    (0..red_tiles.len() - 1)
-        .map(|i| (red_tiles[i], red_tiles[i + 1]))
-        .any(|(p1, p2)| between(&p1, &p2, candidate))
-}
-
-fn between(p1: &Corner, p2: &Corner, candidate: &Corner) -> bool {
-    if p1[0] == p2[0] {
-        candidate[1] >= p1[1] && candidate[1] <= p2[1]
-            || candidate[1] >= p2[1] && candidate[1] <= p1[1]
-    } else if p1[1] == p2[1] {
-        candidate[0] >= p1[0] && candidate[0] <= p2[0]
-            || candidate[0] >= p2[0] && candidate[1] <= p1[0]
-    } else {
-        false
     }
 }
