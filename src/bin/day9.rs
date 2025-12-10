bf17fa32ef51a56b153bc52e43a06e8e5f2aa0a4
gjf2a <ferrer@hendrix.edu>
2025-12-09 22:01:45 -0600

    Draft of Part 2 solution, but crashes.

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,4 +1,7 @@
-use std::{cmp::{max, min}, collections::HashSet};
+use std::{
+    cmp::{max, min},
+    collections::HashSet,
+};
 
 use advent2025::{Part, advent_main, all_lines, multidim::Point, sub_abs};
 use itertools::Itertools;
@@ -9,54 +12,29 @@ type Edge = (Corner, Corner);
 fn main() -> anyhow::Result<()> {
     advent_main(|filename, part, _| {
         let red_tiles = get_red_tiles(filename)?;
+        let rects = all_rectangles(&red_tiles);
         match part {
             Part::One => {
-                println!("{}", largest_rectangle_area(&red_tiles));
+                let biggest = rects
+                    .iter()
+                    .map(|(p1, p2)| rectangle_area(p1, p2))
+                    .max()
+                    .unwrap();
+                println!("{biggest}");
             }
             Part::Two => {
-                let everyone = all_rectangles(&red_tiles);
-                let no_crossings = no_crossing_rectangles(&red_tiles);
-                let no_inside = no_inside_rectangles(&red_tiles);
-                let neither = no_inside.iter().filter(|r| no_crossings.contains(r)).collect_vec();
-                /*for candidate in everyone.iter() {
-                    println!("{candidate:?} no crossings? {} no inside? {}", no_crossings.contains(candidate), no_inside.contains(candidate));
-                }*/
-                println!("Everyone: {}", everyone.len());
-                println!("No crossings: {}", no_crossings.len());
-                println!("No inside: {}", no_inside.len());
-                println!("Neither: {}", neither.len());
-                for candidate in neither.iter() {
-                    println!("{candidate:?} {}", rectangle_area(&candidate.0, &candidate.1));
-                }
-                let largest_available = neither.iter().map(|r| rectangle_area(&r.0, &r.1)).max().unwrap();
-                println!("{largest_available}");
+                let edges = RedEdges::new(&red_tiles);
+                let biggest = rects
+                    .iter()
+                    .filter(|(p1, p2)| edges.rectangle_approved(p1, p2))
+                    .map(|(p1, p2)| rectangle_area(p1, p2))
+                    .max()
+                    .unwrap();
+                println!("{biggest}");
             }
         }
         Ok(())
     })
-}
-
-fn get_red_tiles(filename: &str) -> anyhow::Result<Vec<Point<u64, 2>>> {
-    Ok(all_lines(filename)?
-        .map(|line| line.parse::<Corner>().unwrap())
-        .collect())
-}
-
-fn largest_rectangle_area(red_tiles: &Vec<Corner>) -> u64 {
-    let mut biggest = 0;
-    for i in 0..red_tiles.len() {
-        for j in i + 1..red_tiles.len() {
-            biggest = max(biggest, rectangle_area(&red_tiles[i], &red_tiles[j]))
-        }
-    }
-    biggest
-}
-
-fn rectangle_area(p1: &Corner, p2: &Corner) -> u64 {
-    p1.values()
-        .zip(p2.values())
-        .map(|(v1, v2)| 1 + sub_abs(v1, v2))
-        .product()
 }
 
 fn all_rectangles(red_tiles: &Vec<Corner>) -> Vec<(Corner, Corner)> {
@@ -68,6 +46,129 @@ fn all_rectangles(red_tiles: &Vec<Corner>) -> Vec<(Corner, Corner)> {
     }
     result
 }
+
+fn get_red_tiles(filename: &str) -> anyhow::Result<Vec<Point<u64, 2>>> {
+    Ok(all_lines(filename)?
+        .map(|line| line.parse::<Corner>().unwrap())
+        .collect())
+}
+
+fn rectangle_area(p1: &Corner, p2: &Corner) -> u64 {
+    p1.values()
+        .zip(p2.values())
+        .map(|(v1, v2)| 1 + sub_abs(v1, v2))
+        .product()
+}
+
+// Part 2 ideas
+
+// Possible idea:
+// * Is a Corner green?
+//   * Examine its y coordinate. Is there an outer edge with that y?
+//     * If so, what is the lowest x coordinate?
+//       * March across the y row starting at the low x, finding another x at that
+//         y that is on an edge. Is our Corner's x between them? If so, great!
+//         If not, keep looking at this row for another edge to restart the process.
+
+struct RedEdges {
+    edges: Vec<LevelInterval>,
+}
+
+impl RedEdges {
+    fn new(red_tiles: &Vec<Corner>) -> Self {
+        let mut edges = vec![];
+        for i in 0..red_tiles.len() - 1 {
+            let p1 = red_tiles[i];
+            let p2 = red_tiles[i + 1];
+            if p1[0] == p2[0] {
+                edges.push(LevelInterval {
+                    level: p1[0],
+                    start: min(p1[1], p2[1]),
+                    end: max(p1[1], p2[1]),
+                    tilt: Tilt::Vertical,
+                });
+            } else if p1[1] == p2[1] {
+                edges.push(LevelInterval {
+                    level: p1[1],
+                    start: min(p1[0], p2[0]),
+                    end: max(p1[0], p2[0]),
+                    tilt: Tilt::Horizontal,
+                });
+            } else {
+                panic!("Invalid red-tiles list!");
+            }
+        }
+        Self { edges }
+    }
+
+    fn rectangle_approved(&self, p1: &Corner, p2: &Corner) -> bool {
+        let p12 = Point::new([p1[0], p2[1]]);
+        let p21 = Point::new([p1[1], p2[0]]);
+        self.is_green(&p12) && self.is_green(&p21)
+    }
+
+    fn is_green(&self, corner: &Corner) -> bool {
+        let mut inside = false;
+        for edge in self.edges_at(corner[1]) {
+            match edge.tilt {
+                Tilt::Horizontal => {
+                    inside = true;
+                    if edge.contains(&corner[0]) {
+                        return true;
+                    }
+                }
+                Tilt::Vertical => {
+                    if inside {
+                        if corner[0] <= edge.level {
+                            return true;
+                        }
+                        inside = false;
+                    } else {
+                        if corner[0] == edge.level {
+                            return true;
+                        } else if corner[0] < edge.level {
+                            return false;
+                        }
+                        inside = true;
+                    }
+                }
+            }
+        }
+        inside
+    }
+
+    fn edges_at(&self, y: u64) -> impl Iterator<Item = LevelInterval> {
+        self.edges
+            .iter()
+            .filter(move |edge| match edge.tilt {
+                Tilt::Horizontal => y == edge.level,
+                Tilt::Vertical => edge.start <= y && y <= edge.end,
+            })
+            .copied()
+    }
+}
+
+#[derive(Copy, Clone)]
+enum Tilt {
+    Horizontal,
+    Vertical,
+}
+
+#[derive(Copy, Clone)]
+struct LevelInterval {
+    level: u64,
+    start: u64,
+    end: u64,
+    tilt: Tilt,
+}
+
+impl LevelInterval {
+    fn contains(&self, v: &u64) -> bool {
+        self.start <= *v && *v <= self.end
+    }
+}
+
+// Older Part 2 ideas
 
 fn no_crossing_rectangles(red_tiles: &Vec<Corner>) -> HashSet<(Corner, Corner)> {
     let mut result = HashSet::new();
