527151f58dd267bdc998bcaa871edb8de92bd8dc
gjf2a <ferrer@hendrix.edu>
2025-12-11 12:55:42 -0600

    Cleaned up code a bit, including making the Part 2 supplemental output clearer.

--- src/bin/day11.rs
+++ src/bin/day11.rs
@@ -9,17 +9,28 @@ fn main() -> anyhow::Result<()> {
         let graph = graph_from_file(filename)?;
         match part {
             Part::One => {
-                let count = PathsEndingAtTable::path_count(graph.reversed(), "you", "out");
+                let count = PathsEndingAtTable::path_count(&graph.reversed(), "you", "out");
                 println!("{count}");
             }
             Part::Two => {
-                let svr2fft = PathsEndingAtTable::path_count(graph.without("dac").reversed(), "svr", "fft");
-                let fft2dac = PathsEndingAtTable::path_count(graph.reversed(), "fft", "dac");
-                let dac2out = PathsEndingAtTable::path_count(graph.without("fft").reversed(), "fft", "out");
-                let svr2dac = PathsEndingAtTable::path_count(graph.without("fft").reversed(), "svr", "dac");
-                let dac2fft = PathsEndingAtTable::path_count(graph.reversed(), "dac", "fft");
-                let fft2out = PathsEndingAtTable::path_count(graph.without("dac"), "fft", "out");
-                let total = svr2fft + fft2dac + dac2out + svr2dac + dac2fft + fft2out;
+                let reversed = graph.reversed();
+                let no_fft = graph.without("fft").reversed();
+                let no_dac = graph.without("dac").reversed();
+                let svr2fft = PathsEndingAtTable::path_count(&no_dac, "svr", "fft");
+                let fft2dac = PathsEndingAtTable::path_count(&reversed, "fft", "dac");
+                let dac2out = PathsEndingAtTable::path_count(&no_fft, "dac", "out");
+                let svr2dac = PathsEndingAtTable::path_count(&no_fft, "svr", "dac");
+                let dac2fft = PathsEndingAtTable::path_count(&reversed, "dac", "fft");
+                let fft2out = PathsEndingAtTable::path_count(&no_dac, "fft", "out");
+                let svr2fft2dac2out = svr2fft * fft2dac * dac2out;
+                let svr2dac2fft2out = svr2dac * dac2fft * fft2out;
+                println!(
+                    "svr -> fft -> dac -> out: {svr2fft} * {fft2dac} * {dac2out} = {svr2fft2dac2out}",
+                );
+                println!(
+                    "svr -> dac -> fft -> out: {svr2dac} * {dac2fft} * {fft2out} = {svr2dac2fft2out}",
+                );
+                let total = svr2fft2dac2out + svr2dac2fft2out;
                 println!("{total}");
             }
         }
@@ -39,22 +50,20 @@ pub fn graph_from_file(filename: &str) -> anyhow::Result<AdjacencySets> {
     Ok(result)
 }
 
-// paths_ending_at(you) = 1
-// paths_ending_at(n) = sum of paths_ending_at(n') for all n' with an edge into n
 struct PathsEndingAtTable {
     reversed: AdjacencySets,
     ending_at: HashMap<String, u64>,
 }
 
 impl PathsEndingAtTable {
-    fn new(reversed: AdjacencySets, start_node: &str) -> Self {
+    fn new(reversed: &AdjacencySets, start_node: &str) -> Self {
         Self {
-            reversed,
+            reversed: reversed.clone(),
             ending_at: hash_map!(start_node.to_string() => 1),
         }
     }
 
-    fn path_count(reversed: AdjacencySets, start_node: &str, end_node: &str) -> u64 {
+    fn path_count(reversed: &AdjacencySets, start_node: &str, end_node: &str) -> u64 {
         let mut table = Self::new(reversed, start_node);
         table.paths_ending_at(end_node)
     }
@@ -78,8 +87,3 @@ impl PathsEndingAtTable {
         }
     }
 }
-
-// no_dac.paths_starting_ending(svr, fft) + everyone.paths_starting_ending(fft, dac) + no_fft.paths_starting_ending(dac, out)
-// + no_fft.paths_starting_ending(svr, dac) + everyone.paths_starting_ending(dac, fft) + no_dac.paths_starting_ending(fft, out)
-//
-// Use three tables: one with everyone, one without fft, and one without dac.
