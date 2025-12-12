a92c1561811ceaad46e53521df2601056381c9be
gjf2a <ferrer@hendrix.edu>
2025-12-12 16:09:18 -0600

    Drafts of Part 2 ideas.

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,6 +1,7 @@
 use std::cmp::{max, min};
 
 use advent2025::{Part, advent_main, all_lines, multidim::Point, sub_abs};
+use num::Integer;
 
 type Corner = Point<u64, 2>;
 
@@ -50,14 +51,8 @@ fn rectangle_area(p1: &Corner, p2: &Corner) -> u64 {
 fn legit_rectangles(red_tiles: &Vec<Corner>) -> Vec<(Corner, Corner)> {
     let mut result = vec![];
     for i in 0..red_tiles.len() {
-        let red1 = red_tiles[i];
         for j in i + 1..red_tiles.len() {
-            let red2 = red_tiles[j];
-            let others = [(red1[0], red2[1]), (red2[0], red1[1])];
-            if others.iter().all(|(x, y)| {
-                let candidate = Corner::new([*x, *y]);
-                along_some_edge(red_tiles, &candidate)
-            }) {
+            if is_legit(red_tiles, i, j) {
                 result.push((red_tiles[i], red_tiles[j]));
             }
         }
@@ -65,11 +60,27 @@ fn legit_rectangles(red_tiles: &Vec<Corner>) -> Vec<(Corner, Corner)> {
     result
 }
 
-fn along_some_edge(red_tiles: &Vec<Corner>, candidate: &Corner) -> bool {
-    (0..red_tiles.len()).any(|i| along_edge(&red_tiles[i], &red_tiles[(i + 1) % red_tiles.len()], candidate))
+// Concept
+// If a candidate corner is in-bounds, we can look at how it extends past the next points in the sequence.
+// If the current corner is horizontal, we look for the next point that goes below (if going right) or above (if going left).
+// If that next point has an x coordinate greater than our candidate, the candidate is legit.
+//
+// Apply the inverse logic in the y direction.
+//
+fn is_legit(red_tiles: &Vec<Corner>, i: usize, j: usize) -> bool {
+    let red1 = red_tiles[i];
+    let red2 = red_tiles[j];
+    let pre1 = red_tiles[(i - 1).mod_floor(&red_tiles.len())];
+    assert!(j > 0);
+    let pre2 = red_tiles[j - 1]; 
+
+    red1[0] == red2[0] || red1[1] == red2[1] 
 }
 
-fn along_edge(p1: &Corner, p2: &Corner, candidate: &Corner) -> bool {
-    p1[0] == p2[0] && candidate[0] == p1[0] && min(p1[1], p2[1]) <= candidate[1] && candidate[1] <= max(p1[1], p2[1])
-    || p1[1] == p2[1] && candidate[1] == p1[1] && min(p1[0], p2[0]) <= candidate[0] && candidate[0] <= max(p1[0], p2[0])
+fn find_horizontal_ys(red_tiles: &Vec<Corner>) -> Vec<(usize,u64)> {
+    (0..red_tiles.len()).filter(|i| red_tiles[*i][0] == red_tiles[(i + 1) % red_tiles.len()][0]).map(|i| (i, red_tiles[i][1])).collect()
+}
+
+fn find_vertical_xs(red_tiles: &Vec<Corner>) -> Vec<(usize, u64)> {
+    (0..red_tiles.len()).filter(|i| red_tiles[*i][1] == red_tiles[(i + 1) % red_tiles.len()][1]).map(|i| (i, red_tiles[i][0])).collect()
 }
