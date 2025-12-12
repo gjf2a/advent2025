35e9286a02ead072e313e8038473a6362ecb1d19
gjf2a <ferrer@hendrix.edu>
2025-12-12 15:36:51 -0600

    Day 9, Part 2 solution checks for corners on edge. Ignores interior corners, to its detriment.

--- src/bin/day9.rs
+++ src/bin/day9.rs
@@ -1,31 +1,17 @@
 use std::cmp::{max, min};
 
 use advent2025::{Part, advent_main, all_lines, multidim::Point, sub_abs};
-use itertools::Itertools;
-use num::integer::gcd;
 
 type Corner = Point<u64, 2>;
 
 fn main() -> anyhow::Result<()> {
     advent_main(|filename, part, _| {
         let red_tiles = get_red_tiles(filename)?;
-        let rects = all_rectangles(&red_tiles);
-        match part {
-            Part::One => {
-                println!("{}", largest_rectangle_area(&rects));
-            }
-            Part::Two => {
-                let diffs = (0..red_tiles.len() - 1)
-                    .map(|i| red_tiles[i].manhattan_distance(&red_tiles[i + 1]))
-                    .collect_vec();
-                let mut gcd_diffs = gcd(diffs[0], diffs[1]);
-                for i in 1..diffs.len() {
-                    gcd_diffs = gcd(gcd_diffs, diffs[i]);
-                }
-                println!("diffs: {diffs:?}");
-                println!("diff GCD: {gcd_diffs}");
-            }
-        }
+        let rects = match part {
+            Part::One => all_rectangles(&red_tiles),
+            Part::Two => legit_rectangles(&red_tiles),
+        };
+        println!("{}", largest_rectangle_area(&rects));    
         Ok(())
     })
 }
@@ -59,4 +45,31 @@ fn rectangle_area(p1: &Corner, p2: &Corner) -> u64 {
         .zip(p2.values())
         .map(|(v1, v2)| 1 + sub_abs(v1, v2))
         .product()
+}
+
+fn legit_rectangles(red_tiles: &Vec<Corner>) -> Vec<(Corner, Corner)> {
+    let mut result = vec![];
+    for i in 0..red_tiles.len() {
+        let red1 = red_tiles[i];
+        for j in i + 1..red_tiles.len() {
+            let red2 = red_tiles[j];
+            let others = [(red1[0], red2[1]), (red2[0], red1[1])];
+            if others.iter().all(|(x, y)| {
+                let candidate = Corner::new([*x, *y]);
+                along_some_edge(red_tiles, &candidate)
+            }) {
+                result.push((red_tiles[i], red_tiles[j]));
+            }
+        }
+    }
+    result
+}
+
+fn along_some_edge(red_tiles: &Vec<Corner>, candidate: &Corner) -> bool {
+    (0..red_tiles.len()).any(|i| along_edge(&red_tiles[i], &red_tiles[(i + 1) % red_tiles.len()], candidate))
+}
+
+fn along_edge(p1: &Corner, p2: &Corner, candidate: &Corner) -> bool {
+    p1[0] == p2[0] && candidate[0] == p1[0] && min(p1[1], p2[1]) <= candidate[1] && candidate[1] <= max(p1[1], p2[1])
+    || p1[1] == p2[1] && candidate[1] == p1[1] && min(p1[0], p2[0]) <= candidate[0] && candidate[0] <= max(p1[0], p2[0])
 }
